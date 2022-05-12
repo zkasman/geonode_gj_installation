8bf14c6f8cb2422218221776ceb2a054cdcc0f22
zkasman <zainikasman@gmail.com>
2022-05-12 14:41:02 +0800

    Update install-geonode-part2.sh

--- install-geonode-part2.sh
+++ install-geonode-part2.sh
@@ -6,7 +6,7 @@ sudo chown -Rf $USER:www-data /opt/geonode/
 sudo chmod -Rf 775 /opt/geonode/
 
 cd /opt
-git clone https://github.com/zkasman/geonode.git -b 3.3.x-bw geonode
+git clone https://github.com/zkasman/geonode.git -b 3.3.x-bw2 geonode
 
 cd /opt/geonode
 docker-compose -f docker-compose.yml pull
